f97785636f37f3ae11ad76a85c2cc7ab21653858
Sameer Deshmukh <sameerdeshmukh827@gmail.com>
2024-01-16 01:11:11 +0530

    added fetch secret logic

--- config/DBConnection.js
+++ config/DBConnection.js
@@ -1,13 +1,31 @@
 const mongoose = require("mongoose");
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'ap-south-1' });
+
+// Create a new Secrets Manager client
+const secretsManager = new AWS.SecretsManager();
+
+const connectionSecretKey = process.env.MONGODB_CONNECTION_STRING;
+
+const secretName = 'MONGODB_CONNECTION_SECRET'
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    // Call AWS Secrets Manager to retrieve the secret value
+    const data = await secretsManager.getSecretValue({ SecretId: secretName }).promise();
+    // console.log(data)
+    if('SecretString' in data){
+      const connectionString = JSON.parse(data.SecretString)[connectionSecretKey];
+      // console.log(connectionString);
+      const connect = await mongoose.connect(connectionString);
     console.log(
       "connected to DB",
       connect.connection.host,
       connect.connection.name
     );
+
+    }
+    
   } catch (error) {
     console.log(error);
     process.exit(1);
