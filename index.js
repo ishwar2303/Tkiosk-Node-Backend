424618d9467d518cd1980d889c3f9db11cec7967
ishwar2303 <ib4717@srmist.edu.in>
2024-03-02 13:24:17 +0530

    Logged 12.0 Version Running

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const resolvers = require("./graphql/resolvers");
 const { contextMiddleware } = require("./middlewares/ValidateTokenHandler");
 
 // connectDB();
-console.log('Running Version 11.0 -x-x-x-x-x-x-')
+console.log('Running Version 12.0 -x-x-x-x-x-x-')
 
 //app.use(express.json(), cors(), bodyParser.json());
 const server = new ApolloServer({
