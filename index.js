76875702929d8d450726bcd7ae0c60f7d90c2651
ishwar2303 <ib4717@srmist.edu.in>
2024-03-02 12:49:04 +0530

    Revert "Use 8081 Port Only"
    This reverts commit 17cefa294964418810d669c4819908afeb1067a5.

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const startServer = async () => {
     })
   );
 
-  const port = 8081;
+  const port = process.env.PORT ? process.env.PORT : 8081;
   app.listen(port, () => {
     console.log(`app listening on port ${port}`);
   });
